d961d6e3b7a8f693777a2abb58ecc00c4a1092c5
silvertooth08 <silv3rtooth@gmail.com>
2022-11-19 03:08:58 +0300

    filters emitted from child to parent

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,11 +6,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 cols = 3;
+category: string | undefined;
   constructor() { }
 
   ngOnInit(): void {
   }
   onColumnsCountChange(colsNum: number): void {
   this.cols = colsNum;
+  }
+
+  onShowCategory(newCategory: string) : void {
+   this.category = newCategory;
   }
 }
