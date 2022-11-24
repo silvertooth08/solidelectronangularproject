52f0a299292280d9275179f8a5cf3e6f111d5776
silvertooth08 <silv3rtooth@gmail.com>
2022-11-25 01:24:21 +0300

    increase quantity button logic implemented

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -51,4 +51,8 @@ export class CartComponent implements OnInit {
   onRemoveFromCart(item: CartItem): void {
     this.cartService.removeFromCart(item);
   }
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item);
+  }
+  
 }
