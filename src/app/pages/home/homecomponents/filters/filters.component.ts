a151a092c2fd314f0f9391a113442c0f8941fafa
silvertooth08 <silv3rtooth@gmail.com>
2022-11-25 03:41:25 +0300

    added dynamic category and sort by category(slow)

--- src/app/pages/home/homecomponents/filters/filters.component.ts
+++ src/app/pages/home/homecomponents/filters/filters.component.ts
@@ -1,17 +1,32 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { StoreService } from 'src/app/services/store.service';
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-filters',
   templateUrl:'filters.component.html'
 })
-export class FiltersComponent implements OnInit {
+export class FiltersComponent implements OnInit, OnDestroy{
   @Output() showCategory = new EventEmitter<string>();
-  categories = ['electronics','clothes',]
-  constructor() { }
+  categoriesSubscription: Subscription | undefined;
+  categories : Array<string> | undefined;
+ 
+  constructor(private storeService: StoreService) { }
 
   ngOnInit(): void {
+    this.categoriesSubscription =this.storeService.getAllCategories()
+    .subscribe((response) => {
+      this.categories = response;
+    });
   }
  onShowCategory(category: string) : void {
    this.showCategory.emit(category);
  }
+
+ngOnDestroy(): void {
+  if(this.categoriesSubscription) {
+    this.categoriesSubscription.unsubscribe();
+  }
+}
+
 }
